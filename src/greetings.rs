96a68c03fbfaca5a70560def89c5607e249d6da4
clement32112 <totoobieke@gmail.com>
2023-10-29 20:13:59 +0100

    Added greetings french and spanish

---
+++ src/greetings.rs
@@ -0,0 +1,8 @@
+pub mod spanish;
+pub mod french;
+
+pub fn default_greeting()->String
+{
+    let message = String::from("Hi");
+    message
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod greetings;
+use greetings::{default_greeting, french, spanish};
+
 fn main() {
     println!("Hello world");
+    println!("{}", default_greeting());
+    println!("{}", spanish::default_greeting());
+    println!("{}", french::default_greeting());
 }
