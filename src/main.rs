4e380f7296246a940209be28bb59964bb8b9b8bf
clement32112 <totoobieke@gmail.com>
2023-10-29 20:28:02 +0100

    completed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod greetings;
-use greetings::{default_greeting, french, spanish};
+extern crate new_lib;
 
+use greetings::{default_greeting, french, spanish};
+use new_lib::greeting_from_lib;
 fn main() {
     println!("Hello world");
     println!("{}", default_greeting());
     println!("{}", spanish::default_greeting());
     println!("{}", french::default_greeting());
+    println!("{}", greeting_from_lib());
 }
