596104ac0145d8155c5721173873bf4bc3ede03d
clement32112 <totoobieke@gmail.com>
2023-11-15 17:03:16 +0100

    Structs and implements

--- data_type_example/src/how_to_hold_data/derived/user_derived.rs
+++ data_type_example/src/how_to_hold_data/derived/user_derived.rs
@@ -1,14 +1,49 @@
+
+pub mod enums{
+    enum Nums
+    {
+        ZERO,
+        ONE,
+        TWO,
+        THREE
+    }
+}
+
 #[allow(dead_code)]
-
-
 pub mod structs {
     #[derive(Default,Debug)]
-    
     struct Date {
         day: u8,
         month: u8,
         year: u32,
-       
+
+    }
+    pub struct Point
+    {
+        pub x: i32,
+        pub y:i32
+    }
+    struct Rectangle
+    {
+        top_left:Point,
+        bottom_right:Point
+    }
+pub trait Movable
+    {
+        fn move_left(&mut self,steps:i32);
+
+        fn display_coords(&self);
+
+    }
+
+    impl Movable for Point
+    {
+        fn move_left(&mut self,steps:i32) {
+            self.x +=  steps;
+        }
+        fn display_coords(&self) {
+            print!("x:{} ,y:{}",self.x,self.y)
+        }
     }
 
     #[derive(Default,Debug)]
@@ -17,17 +52,17 @@ pub mod structs {
         age: i32,
         dob: Date,
         marital_status: bool,
-    }
+  }
     pub fn run(){
         let example:UserDefinedStruct = UserDefinedStruct {
-        name:String::from("Clement"),age:12,dob:Date{day:12,month:12,year:12},
-        marital_status:true};
+            name:String::from("Clement"),age:12,dob:Date{day:12,month:12,year:12},
+            marital_status:true};
         println!("{:#?}", example);
-        }
+    }
 }
 
 pub mod union {
-  //#[warn(unused_variables)]
+    //#[warn(unused_variables)]
     #[allow(dead_code)]
     pub union Person {
         id: u8,
@@ -38,7 +73,7 @@ pub mod union {
     }
 
     pub fn run(){
-    let _example:Person = Person{volume: [12,1,3]};
+        let _example:Person = Person{volume: [12,1,3]};
 
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use data_type_example::how_to_hold_data;
 use how_to_hold_data::primitive::{scalar,compound};
 #[allow(unused_imports)]
 use how_to_hold_data::derived::user_derived;
+use how_to_hold_data::derived::user_derived::structs::Movable;
 
 //use data_type_example::how_to_hold_data::primitive::compound;
 use greetings::{english, french, spanish};
@@ -39,9 +40,14 @@ fn greetins(){
     println!("{}", spanish::default_greeting());
     println!("{}", french::default_greeting());
     println!("{}", greeting_from_lib());
-    prin    
 
 }
 fn main() {
     println!("{}", "Hello world");   
-    }
+    let mut my_point: user_derived::structs::Point = user_derived::structs::Point{x:1,y:2};
+    my_point.display_coords();
+    println!("");
+    my_point.move_left(12);
+    my_point.display_coords();
+    
+}
